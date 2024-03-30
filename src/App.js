501b0fb16fafde44c1d2c33c21f109579bb488a5
Nippun Tyagi <nippun@krowdit.com>
2024-03-30 16:25:34 +0530

    this is new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contect from './pages/Contect';
+import Header from './components/Header/Header';
+import Mathura from './pages/Mathura';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <Router>
+      <Header/>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path="/about" element={<About/>}/>
+    <Route path="/contect" element={<Contect/>}/>
+    <Route path="/mathura" element={<Mathura/>}/>
+
+    
+    </Routes>
+    </Router>
+    
   );
 }
 
